04abd01313f1c9245649a5c38cce6b9d789210df
douglaasdg <crisfer_012@outlook.com>
2026-08-21 00:14:19 -0300

    Atualização de configuração de style, adição de fontes e inclusão de lib icons

---
+++ app/src/main/java/br/com/fiap/foodrescue/ui/screens/home/HomeScreen.kt
@@ -0,0 +1,179 @@
+package br.com.fiap.foodrescue.ui.screens.home
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForwardIos
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.NotificationsNone
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.fiap.foodrescue.ui.theme.FoodRescueTheme
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+            .background(color = MaterialTheme.colorScheme.secondaryContainer)
+    ){
+        Scaffold(
+            topBar = {MyTopAppBar()},
+            bottomBar = {},
+        ) { ContentScreen()}
+    }
+}
+
+@Preview(
+    showBackground = true
+)
+@Composable
+private fun HomeScreenPreview() {
+    FoodRescueTheme {
+        HomeScreen()
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTopAppBar(modifier: Modifier = Modifier) {
+    TopAppBar(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp),
+        title = {
+            Box(modifier = Modifier){
+                Column() {
+                    Row() {
+                        Image(
+                            modifier = Modifier
+                                .size(50.dp)
+                                .clip(CircleShape),
+                            painter = painterResource(br.com.fiap.foodrescue.R.drawable.icon_app),
+                            contentDescription = "Icon app"
+                        )
+                        MyLocation()
+                        Spacer(modifier = Modifier.padding(start = 70.dp))
+                        Icon(
+                            imageVector = Icons.Default.NotificationsNone,
+                            contentDescription = "Notification icon",
+                            modifier = Modifier
+                                .padding(8.dp)
+
+
+                        )
+                    }
+                    Spacer(modifier = Modifier.padding(top = 8.dp))
+                    Text(
+                        text = "Home",
+                        color = MaterialTheme.colorScheme.primary,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Preview(
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO
+)
+@Composable
+private fun MyTopAppBarPreview() {
+    FoodRescueTheme() {
+        Surface {
+            MyTopAppBar()
+        }
+    }
+}
+
+@Composable
+fun MyLocation(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .width(200.dp)
+    ){
+        Column(modifier = modifier) {
+            Row(
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Icon(
+                    imageVector = Icons.Default.LocationOn,
+                    contentDescription = "Location icon",
+                    tint = Color.Gray
+                )
+                Text(
+                    modifier = Modifier
+                        .padding(start = 6.dp),
+                    text = "Location",
+                    color = Color.Gray,
+                    style = MaterialTheme.typography.headlineSmall
+                )
+                Icon(
+                    imageVector = Icons.Default.ArrowForwardIos,
+                    contentDescription = "Arrow icon",
+                    tint = Color.Gray
+                )
+            }
+            Row() {
+                Text(
+                    modifier = Modifier
+                        .padding(start = 30.dp),
+                    text = "Vila Madalena, SP",
+                    style = MaterialTheme.typography.displaySmall
+                )
+            }
+        }
+    }
+}
+
+@Preview(
+    showBackground = true
+)
+@Composable
+private fun MyLocationPreview() {
+    FoodRescueTheme() {
+        MyLocation()
+    }
+}
+
+@Composable
+fun ContentScreen(modifier: Modifier = Modifier) {
+    
+}
+
+@Preview
+@Composable
+private fun ContentScreenPreview() {
+    FoodRescueTheme() {
+        ContentScreen()
+    }
+}
