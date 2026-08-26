8ed78713e17b1e6507ab123581cd5bd11beeb1cd
douglaasdg <crisfer_012@outlook.com>
2026-08-25 21:40:20 -0300

    Adição da lib navigation, initial screen, metodos de navegação

---
+++ app/src/main/java/br/com/fiap/foodrescue/ui/navigation/ScreenRoutes.kt
@@ -0,0 +1,16 @@
+package br.com.fiap.foodrescue.ui.navigation
+
+sealed class Destination(val route: String){
+
+    object InitialScreen: Destination("Initial")
+
+    object LoginScreen: Destination("register")
+
+    object RegisterScreen: Destination("Signup")
+
+    object HomeScreen: Destination("home")
+
+    object ExploreScreen: Destination("explore")
+
+    object PickUpScreen: Destination("pickup")
+}
